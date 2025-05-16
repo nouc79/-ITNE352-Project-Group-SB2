4b483f823624e9d55735a2fe8fc3911b8718b420
nouc79 <n.aljabor1421@gmail.com>
2025-05-16 23:42:15 +0300

    ne352

--- client_gui.py
+++ client_gui.py
@@ -1,8 +1,19 @@
 import tkinter as tk 
+import socket
 
 root = tk.Tk()
+
+# I will connect to the server 
+client_socket = socket.socket()
+client_socket.connect(('127.0.0.1', 3522)) 
+client_socket.sendall(b"Client")  
+
 #for window size
 root.geometry("800x500")
+
 #make title for this window
-root.title("ITNE352")
-root.mainloop()
+root.title("Flight-ITNE352")
+
+
+
+
